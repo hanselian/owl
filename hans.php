229be07273820d7fbb4f350e508c374a839d2a6c
Hans Elian <202403037@student.stikomyos.ac.id>
2025-12-16 09:49:23 +0700

    program aritmatika

---
+++ hans.php
@@ -0,0 +1,43 @@
+#program aritmatika sederhana dalam PHP
+
+<?php
+#penjumlahan dua angka
+$a = 10;
+$b = 5;
+$jumlah = $a + $b;
+echo "Hasil penjumlahan dari $a + $b = $jumlah";
+?>
+
+<?php
+#pengurangan dua angka
+$a = 10;
+$b = 5;
+$kurang = $a - $b;
+echo "Hasil pengurangan dari $a - $b = $kurang";
+?>
+
+<?php
+#perkalian dua angka
+$a = 10;
+$b = 5;
+$kali = $a * $b;
+echo "Hasil perkalian dari $a * $b = $kali";
+?>
+
+<?php
+#pembagian dua angka
+$a = 10;
+$b = 5;
+$bagi = $a / $b;
+echo "Hasil pembagian dari $a / $b = $bagi";
+?>
+
+<?php
+#modulus dua angka
+$a = 10;
+$b = 5;
+$modulus = $a % $b;
+echo "Hasil modulus dari $a % $b = $modulus";
+?>
+
+
